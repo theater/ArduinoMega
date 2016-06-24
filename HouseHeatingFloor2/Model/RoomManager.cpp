79040378c15d15339108f4583bdf67395eb276b1
polychronov <polychronov@gmail.com>
2016-06-24 07:27:38 +0300

    Signed-off-by: polychronov <polychronov@gmail.com>

---
+++ HouseHeatingFloor2/Model/RoomManager.cpp
@@ -0,0 +1,18 @@
+/*
+ * RoomManager.cpp
+ *
+ *  Created on: 24.06.2016 ã.
+ *      Author: theater
+ */
+
+#include "RoomManager.h"
+
+RoomManager::RoomManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RoomManager::~RoomManager() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ HouseHeatingFloor2/Model/RoomManager.h
@@ -0,0 +1,18 @@
+/*
+ * RoomManager.h
+ *
+ *  Created on: 24.06.2016 ã.
+ *      Author: theater
+ */
+
+#ifndef MODEL_ROOMMANAGER_H_
+#define MODEL_ROOMMANAGER_H_
+
+class RoomManager {
+public:
+
+	RoomManager();
+	virtual ~RoomManager();
+};
+
+#endif /* MODEL_ROOMMANAGER_H_ */
