9b8f735a4f780da94fec0a7067e62e7f69e4f0f7
polychronov <polychronov@gmail.com>
2016-06-27 20:11:26 +0300

    Added wardrobe
    Signed-off-by: polychronov <polychronov@gmail.com>

---
+++ HouseHeatingFloor2/Model/Wardrobe.cpp
@@ -0,0 +1,51 @@
+/*
+ * Wardrobe.cpp
+ *
+ *  Created on: 27.06.2016 ã.
+ *      Author: theater
+ */
+
+#include "Wardrobe.h"
+
+Wardrobe::Wardrobe(PubSubClient* mqttClient, bool DEBUG) : Room(id, mqttClient) {
+	setDebug(DEBUG);
+			if(Debug()) {
+				mqttClient->publish("DEBUG","Wardrobe::Wardrobe()");
+			}
+
+			// initialize and create Output controllers
+			radiatorOne = new OutputControl(WARDROBE_RAD, OFF, RAD_WARDROBE_CB, mqttClient);
+			setHasHeatingControl(true);
+			// initialize and create sensors
+			Sensor* tempSensor = createSensor(TEMPERATURE, mqttClient, SENSOR_WARDROBE_01);
+			setTempSensor((TemperatureSensor*)tempSensor);
+
+			// Set MQTT topics to listen to...
+			setMqttTopics(topics);
+			setLen(length);
+
+			subscribeMqttTopics(mqttClient);
+}
+
+Wardrobe::~Wardrobe() {
+	delete radiatorOne;
+}
+
+// Virtual functions - defined here the outputs associated with specific services (heat, chill, humidity)
+void Wardrobe::heatOutputs(bool state) {
+	radiatorOne->setPin(state);
+}
+
+void Wardrobe::mqttReceive(const char* topic, const char* payload) {
+	String strTopic = String(topic);
+	String strPayload = String(payload);
+	if (strTopic.equals(MODE_WARDROBE)) {
+		updateMode(payload);
+	} else if (strTopic.equals(DESIRED_TEMP_WARDROBE_01)) {
+		updateDesiredTemperature(atof(payload));
+	} else if (strTopic.equals(RAD_WARDROBE)) {
+		handleMqttCommandOC(radiatorOne, payload);
+	} else {
+		getMqttClient()->publish("DEBUG", "No matching rules found");
+	}
+}
---
+++ HouseHeatingFloor2/Model/Wardrobe.h
@@ -0,0 +1,35 @@
+/*
+ * Wardrobe.h
+ *
+ *  Created on: 27.06.2016 ã.
+ *      Author: theater
+ */
+
+#ifndef MODEL_WARDROBE_H_
+#define MODEL_WARDROBE_H_
+
+#include <Room.h>
+#include "../Config/Config.h"
+
+class Wardrobe: public Room {
+	const static RoomId id = WARDROBE;
+
+	OutputControl* radiatorOne;
+
+	//TODO: ugly - fix it somehow later
+	const static int length = 3;
+	const char * topics[length] = { RAD_WARDROBE,
+	DESIRED_TEMP_WARDROBE_01,
+	MODE_WARDROBE };
+public:
+	Wardrobe(PubSubClient* mqttClient, bool DEBUG = false);
+	virtual ~Wardrobe();
+	void updateOutputControllers();
+	void mqttReceive(const char* strTopic, const char* strPayload);
+	void heatOutputs(bool state);
+	void chillOutputs(bool state) {};
+	void humidityOutputs(bool state) {};
+	;
+};
+
+#endif /* MODEL_WARDROBE_H_ */
